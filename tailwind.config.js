a360a4b8f0cabcf07e98cb791041b9a6fa4b4542
Tatsu24 <67832951+Tatsu24@users.noreply.github.com>
2023-03-30 02:29:37 +0900

    feat: tailwind.config.jsのcontentを追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./*.{vue,js,ts,jsx,tsx}",
+    "./components/**/*.{vue,js,ts,jsx,tsx}",
+    "./layouts/**/*.{vue,js,ts,jsx,tsx}",
+    "./pages/**/*.{vue,js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
